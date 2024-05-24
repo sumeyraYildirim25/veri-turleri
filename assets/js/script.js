153a49661f531d53a8a6194f03b9ad0b252a8150
sumeyraYildirim25 <150522704+sumeyraYildirim25@users.noreply.github.com>
2024-05-24 23:08:10 +0300

    tüm veri tipleri tamamlandı

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 //İlkel Veri Tipleri
-//----string----
+//-----string----
 let name = "sümeyra";
 let city = "istanbul";
 console.log(typeof city);
@@ -56,6 +56,24 @@ let d = new Date(2024, 5, 23);
 console.log(typeof d);
 
 //----RegExp-----
+let text = "Güzel Araba";
+let n = text.search(/Araba/i);
+console.log(typeof n); // number 
+
+let text2 = "Sümeyra Yıldırım";
+let result = text2.replace("Yıldırım", "Öztürk");
+console.log(typeof result); //string
+
 //---map---
+let meyveler = new Map([
+    ["elma", 300],
+    ["muz", 400],
+    ["portakal", 500]
+]);
+console.log(typeof meyveler); //object
+
 //---set----
-//hocam RegExp,Map,Set öğrenmedik yine de yapalım mı?
+meyveler.set("kayısı", 300);
+meyveler.set("armut", 400);
+meyveler.set("vişne", 500);
+console.log(meyveler);
